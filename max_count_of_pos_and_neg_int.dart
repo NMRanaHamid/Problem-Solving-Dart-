f92be04f7511ada334c1089d3ed79a6976f5475b
Rana Hamid <ranahmd02@gmail.com>
2025-03-12 22:20:20 +0600

    initial commit

---
+++ max_count_of_pos_and_neg_int.dart
@@ -0,0 +1,12 @@
+class Solution {
+  int maximumCount(List<int> nums) {
+        int pos=0, neg=0;
+        for(int i=0; i<nums.length; i++)
+        {
+            if(nums[i]>0) pos++;
+            else if(nums[i]<0) neg++ ;
+        }
+        if(pos>neg) return pos ;
+        else return neg ;
+  }
+}
